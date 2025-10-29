f293db72dd3419924c39ab6dea48d5115b33d627
Arindam Roy <arindam_ug_23@ei.nits.ac.in>
2025-10-30 00:39:24 +0530

    feat: Implement signup functionality with validation and error handling

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,14 @@
+const { check, validationResult } = require("express-validator");
 
-exports.getLogin = (req,res,next)=>{
-  res.render("auth/login",{
-    pageTitle:"Login",
-    currentPage:"login",
+exports.getLogin = (req, res, next) => {
+  res.render("auth/login", {
+    pageTitle: "Login",
+    currentPage: "login",
     isLoggedIn: req.isLoggedIn
   });
 }
 
-exports.postLogin = (req,res,next)=>{
+exports.postLogin = (req, res, next) => {
   // const email = req.body.email;
   // const password = req.body.password;
   // res.cookie("isLoggedIn",true);
@@ -15,7 +16,7 @@ exports.postLogin = (req,res,next)=>{
   res.redirect("/");
 }
 
-exports.postLogout = (req,res,next)=>{
+exports.postLogout = (req, res, next) => {
   req.session.destroy((err) => {
     if (err) {
       console.log("Error destroying session:", err);
@@ -25,10 +26,87 @@ exports.postLogout = (req,res,next)=>{
 }
 
 
-exports.getSignup = (req,res,next)=>{
-  res.render("auth/signup",{
-    pageTitle:"Signup",
-    currentPage:"signup",
-    isLoggedIn: req.isLoggedIn
-  }); 
+exports.getSignup = (req, res, next) => {
+  res.render("auth/signup", {
+    pageTitle: "Signup",
+    currentPage: "signup",
+    isLoggedIn: req.isLoggedIn,
+    errors: [],
+    oldInput: {}
+  });
 }
+
+exports.postSignup = [
+  check('firstName')
+    .notEmpty()
+    .withMessage('First name is required')
+    .isLength({ min: 2 })
+    .withMessage('First name must be at least 2 characters long')
+    .isAlpha()
+    .withMessage('First name must contain only letters'),
+
+  check('lastName')
+    .notEmpty()
+    .withMessage('Last name is required')
+    .isLength({ min: 2 })
+    .withMessage('Last name must be at least 2 characters long')
+    .isAlpha()
+    .withMessage('Last name must contain only letters'),
+
+  check('email')
+    .isEmail()
+    .withMessage('Please enter a valid email address')
+    .normalizeEmail(),
+
+  check('password')
+    .isLength({ min: 6 })
+    .withMessage('Password must be at least 6 characters long')
+    .matches(/[a-z]/)
+    .withMessage('Password must contain at least one lowercase letter')
+    .matches(/[A-Z]/)
+    .withMessage('Password must contain at least one uppercase letter')
+    .matches(/[0-9]/)
+    .withMessage('Password must contain at least one number')
+    .matches(/[\W_]/)
+    .withMessage('Password must contain at least one special character')
+    .trim(),
+
+  check('confirmPassword')
+    .custom((value, { req }) => {
+      if (value != req.body.password) {
+        throw new Error('Passwords do not match');
+      }
+      return true;
+    })
+  ,
+  check('role')
+    .notEmpty()
+    .withMessage('Role is required')
+    .isIn(['host', 'guest'])
+    .withMessage('Role must be either host or guest')
+  ,
+  check('terms')
+    .notEmpty()
+    .withMessage('You must accept the terms and conditions')
+    .custom(value => {
+      if (value !== 'on') {
+        throw new Error('You must accept the terms and conditions');
+      }
+      return true;
+    })
+  ,
+  (req, res, next) => {
+    const { firstName, lastName, email, password, role } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).render("auth/signup", {
+        pageTitle: "Signup",
+        currentPage: "signup",
+        isLoggedIn: req.isLoggedIn,
+        errors: errors.array().map(err => err.msg),
+        oldInput: { firstName, lastName, email, password, role }
+      });
+    }
+    res.redirect("/login");
+  }
+]
