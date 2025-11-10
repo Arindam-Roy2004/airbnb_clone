5c9baf6f3e866f0175fd40861c6242d5cf223b6f
Arindam Roy <arindam_ug_23@ei.nits.ac.in>
2025-11-10 15:39:07 +0530

    fix: add authentication check for posting to favourites list

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -189,8 +189,8 @@ exports.postFavouriteList = async (req, res, next) => {
   try {
     const homeId = req.body.homeId;
     if (!homeId) return res.status(400).send("Home ID is required");
+    if (!req.session.user._id) return res.status(401).send("User not authenticated");
     const userId = req.session.user._id;
-    if (!userId) return res.status(401).send("User not authenticated");
 
     const User = await user.findById(userId);
     if (!User) return res.status(404).send("User not found");
